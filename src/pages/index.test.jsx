21a33106dac13b47daf4737af7e1db2c6294d307
Stephen Castaneda <stephencastaneda@Stephens-MacBook-Pro.local>
2022-06-14 12:17:45 -0700

    removed commented out code

--- src/pages/index.test.jsx
+++ src/pages/index.test.jsx
@@ -1,8 +1,6 @@
 import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import {createHome} from "../../pages/index";
-
-
-
+ 
 describe("The Home Page Component", () => {
   const config = {
     title: "Stephen Castaneda | Blog",
@@ -35,29 +33,4 @@ describe("The Home Page Component", () => {
     const posts = screen.getAllByTestId("post");
     expect(posts).toHaveLength(1);
   });
-
-    // Each test for the component will get an `it` block
-    // it("should catch error", async () => {
-    //   // The getByRole will error if there are less or more than 1 element found
-    //   function mockError() {
-    //     return Promise.reject([
-    //       {
-    //         id: 1, 
-    //         date: "2022-02-25", 
-    //         title: "Blog Test",
-    //         author: "Blog Author",
-    //         text: "Hey, hope this test passed!",
-    //       }
-    //     ])
-    //   }
-    //   const Home = createHome(mockError)
-    //   render(
-    //     <Home
-    //       config={config}
-    //     />
-    //   );
-    //   await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
-    //   const posts = screen.getAllByTestId("post");
-    //   expect(posts).toHaveLength(1);
-    // });
 });
